198d83730766dd27d92733b783376dbb299503ff
Elias Hawa <elias.hawa2468@gmail.com>
2026-03-21 22:51:27 -0400

    More clean up and add real time and date to display

--- charging-stand-clock.ino
+++ charging-stand-clock.ino
@@ -1,4 +1,3 @@
-/* Includes ------------------------------------------------------------------*/
 #include <stdlib.h>
 #include "DEV_Config.h"
 #include "EPD.h"
@@ -7,23 +6,26 @@
 #include "imagepainter.h"
 #include "wifihelper.h"
 #include "datetimehelper.h"
+#include "weatherhelper.h"
 
 DateTimeDay datetimedayinfo;
+Weather weather;
+uint8_t last_hour = 0;
+//Create a new image cache
+UBYTE *Canvas;
+char date_buf[30];
+
 
 void setup() {
   DEV_Module_Init();
-
-  printf("e-Paper Init and Clear...\r\n");
   EPD_4IN2_V2_Init();
   EPD_4IN2_V2_Clear();
   DEV_Delay_ms(500);
+  Serial.begin(9600);
 
-  //Create a new image cache
-  UBYTE *BlackImage;
-  
-  UWORD Imagesize = ((EPD_4IN2_V2_WIDTH % 8 == 0)? (EPD_4IN2_V2_WIDTH / 8 ): (EPD_4IN2_V2_WIDTH / 8 + 1)) * EPD_4IN2_V2_HEIGHT;
-  if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-      printf("Failed to apply for black memory...\r\n");
+  UWORD CanvasSize = ((EPD_4IN2_V2_WIDTH % 8 == 0)? (EPD_4IN2_V2_WIDTH / 8 ): (EPD_4IN2_V2_WIDTH / 8 + 1)) * EPD_4IN2_V2_HEIGHT;
+  if((Canvas = (UBYTE *)malloc(CanvasSize)) == NULL) {
+      printf("Failed to allocate memory...\r\n");
       while (1);
   }
   Paint_NewImage(Canvas, EPD_4IN2_V2_WIDTH, EPD_4IN2_V2_HEIGHT, 0, WHITE);
@@ -33,8 +35,24 @@ void setup() {
   wifi_connect();
   time_init();
   // wifi_disconnect();
+  EPD_4IN2_V2_Display(Canvas);
+  Paint_SetRotate(ROTATE_270);
 }
 
 void loop() {
   get_local_date_time(&datetimedayinfo);
+  Paint_Clear(WHITE);
+  memset(date_buf, 0, sizeof date_buf);
+  sprintf(date_buf, "%s %s %u %u", day_names[datetimedayinfo.weekday], month_names[datetimedayinfo.datetime.Month], datetimedayinfo.datetime.Day + 1, datetimedayinfo.datetime.Year);
+  Paint_DrawTime(25, 20, &(datetimedayinfo.datetime), &FontCascadia3, WHITE, BLACK);
+  Paint_DrawString_EN(255, 50, datetimedayinfo.amorpm, &Font20, WHITE, BLACK);
+  Paint_DrawString_EN(45, 80, date_buf, &Font20, WHITE, BLACK);
+  if (last_hour != datetimedayinfo.datetime.Hour) {
+    Serial.println("UPDATING WEATHER");
+    last_hour = datetimedayinfo.datetime.Hour;
+    update_weather(&weather);
+  }
+
+  EPD_4IN2_V2_PartialDisplay(Canvas, 0, 0, EPD_4IN2_V2_WIDTH, EPD_4IN2_V2_HEIGHT);
+  DEV_Delay_ms(250);
 }

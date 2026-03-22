68181b0b981bf74dc87aeb23e28808f8f717dbca
Elias Hawa <elias.hawa2468@gmail.com>
2026-03-21 22:49:30 -0400

    Add check for am and pm

--- datetimehelper.cpp
+++ datetimehelper.cpp
@@ -1,3 +1,4 @@
+#include <stdint.h>
 // Helper functions for getting time
 #include <stdio.h>
 #include <WiFi.h>
@@ -5,7 +6,7 @@
 #include "datetimehelper.h"
 
 const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 0;
+const long  gmtOffset_sec = 3600 * -5;
 const int   daylightOffset_sec = 3600;
 
 void time_init() {
@@ -21,4 +22,25 @@ void get_local_date_time(DateTimeDay* datetimeinfo) {
   datetimeinfo->datetime.Year = timeinfo.tm_year + 1900;
   datetimeinfo->datetime.Day = timeinfo.tm_mday;
   datetimeinfo->datetime.Month = timeinfo.tm_mon;
+  datetimeinfo->datetime.Hour = timeinfo.tm_hour;
+  datetimeinfo->datetime.Min = timeinfo.tm_min;
+  datetimeinfo->datetime.Sec = timeinfo.tm_sec;
+  datetimeinfo->weekday = timeinfo.tm_wday;
+  
+  if (USE_12_HOUR){
+    if (datetimeinfo->datetime.Hour >= 0 && datetimeinfo->datetime.Hour <= 11){
+      strcpy(datetimeinfo->amorpm, "AM");
+    } else {
+      strcpy(datetimeinfo->amorpm, "PM");
+    }
+    if (datetimeinfo->datetime.Hour == 0) {
+      datetimeinfo->datetime.Hour = 12;
+    } else if (datetimeinfo->datetime.Hour > 12) {
+      datetimeinfo->datetime.Hour -= 12;
+    }
+  }
+}
+
+void convert_to_12hr(PAINT_TIME* time) {
+
 }
--- datetimehelper.h
+++ datetimehelper.h
@@ -1,11 +1,17 @@
 #ifndef _DATETIMEHELPER_H_
 #define _DATETIMEHELPER_H_
 
+#define USE_12_HOUR true
+
 #include "GUI_Paint.h"
+
+static const char* day_names[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+static const char* month_names[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dev"};
 
 typedef struct {
   PAINT_TIME datetime;
   int weekday;
+  char amorpm[3];
 } DateTimeDay;
 
 void time_init();

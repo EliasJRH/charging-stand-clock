6288b11a07f86fe7f37b8eb617f89197df776e26
Elias Hawa <elias.hawa2468@gmail.com>
2026-04-03 13:38:48 -0400

    Add weather info

--- charging-stand-clock.ino
+++ charging-stand-clock.ino
@@ -14,6 +14,9 @@ uint8_t last_hour = 25;
 //Create a new image cache
 UBYTE *Canvas;
 char date_buf[30];
+char temp_buf[5];
+char feels_like_buf[20];
+char min_max_buf[20];
 
 
 void setup() {
@@ -42,6 +45,8 @@ void setup() {
 void loop() {
   get_local_date_time(&datetimedayinfo);
   Paint_Clear(WHITE);
+
+  // Date and time
   memset(date_buf, 0, sizeof date_buf);
   sprintf(date_buf, "%s %s %u %u", day_names[datetimedayinfo.weekday], month_names[datetimedayinfo.datetime.Month], datetimedayinfo.datetime.Day + 1, datetimedayinfo.datetime.Year);
   Paint_DrawTime(25, 20, &(datetimedayinfo.datetime), &FontCascadia3, WHITE, BLACK);
@@ -54,6 +59,17 @@ void loop() {
     last_hour = datetimedayinfo.datetime.Hour;
     update_weather(&weather);
   }
+  Paint_DrawNum(90, 120, weather.temp, &FontCascadia3, BLACK, WHITE);
+  Paint_DrawCircle(170, 135, 4, BLACK, DOT_PIXEL_2X2, DRAW_FILL_EMPTY);
+  Paint_DrawChar(180, 120, 'C', &FontCascadia3, BLACK, WHITE);
+
+  memset(feels_like_buf, 0, sizeof feels_like_buf);
+  sprintf(feels_like_buf, "Feels like: %u", weather.feels_like);
+  Paint_DrawString_EN(55, 180, feels_like_buf, &Font20, WHITE, BLACK);
+
+  memset(min_max_buf, 0, sizeof min_max_buf);
+  sprintf(min_max_buf, "High: %u | Low: %u", weather.max, weather.min);
+  Paint_DrawString_EN(30, 200, min_max_buf, &Font20, WHITE, BLACK);
 
   EPD_4IN2_V2_PartialDisplay(Canvas, 0, 0, EPD_4IN2_V2_WIDTH, EPD_4IN2_V2_HEIGHT);
   DEV_Delay_ms(250);

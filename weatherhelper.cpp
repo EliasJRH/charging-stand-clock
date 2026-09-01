719d2de59937026ae4eb73305d3cfa8a3553bd10
Elias Hawa <elias.hawa2468@gmail.com>
2026-08-31 23:13:19 -0400

    Fix var typo

--- weatherhelper.cpp
+++ weatherhelper.cpp
@@ -6,7 +6,7 @@
 
 String weather_response;
 const char* weather_host = "api.open-meteo.com";
-const char* curr_weather_path = "/v1/forecast?latitude=lat&longitude=lon&daily=temperature_2m_max,temperature_2m_min&models=gem_seamless&current=temperature_2m,apparent_temperature&timezone=auto&forecast_days=1";
+const char* local_weather_endpoint = "/v1/forecast?latitude=lat&longitude=lon&daily=temperature_2m_max,temperature_2m_min&models=gem_seamless&current=temperature_2m,apparent_temperature&timezone=auto&forecast_days=1";
 
 bool update_weather(Weather* weather){
   String payload = httpGETRequest(weather_host, local_weather_endpoint, {});
